40ad0978bef6eb2c3ee6ce639029968ae2e1de86
duraanos <duran364@anadolu.edu.tr>
2024-12-13 12:26:52 +0300

    Fix a bug with the number coming from the input isn't a number

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setUserInput(prevUserInput => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: nextValue,
+        [inputIdentifier]: +nextValue,
       };
     });
   }
