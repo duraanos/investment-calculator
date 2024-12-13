61cd4505ad2aa0a458db07db0a2bcc45bf0e7f32
duraanos <duran364@anadolu.edu.tr>
2024-12-13 11:30:32 +0300

    Add the Results component to the root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
   return (
     <>
       <Header />;
       <UserInput />
+      <Results />
     </>
   );
 }
